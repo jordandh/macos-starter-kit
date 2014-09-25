cccfa97bf5097a3663e7be046ae4b3a3cb8d1070
Jordan Harrison <jordandh@gmail.com>
2014-09-25 14:47:03 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Before running this cript give permissions to it first
+# chmod u+x <script path>
+
+# Install homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
