48f04cecd700ef11c95c30d8b07bb96a19a434ed
Jordan Harrison <jordandh@gmail.com>
2014-09-25 15:36:32 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,33 @@
 # Before running this cript give permissions to it first
 # chmod u+x <script path>
 
+
+#
 # Install homebrew
+#
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Make sure homebrew is ready to use
+brew doctor
+
+
+#
+# Install node
+#
+brew install node
+
+
+#
+# Install grunt
+#
+npm install -g grunt-cli
+
+#
+# Install gulp
+#
+npm install -g gulp
+
+#
+# Install bower
+#
+npm install -g bower
